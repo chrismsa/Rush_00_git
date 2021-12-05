6a96fce05e795c5ab764ea692c0ba1a1a6371861
jeluiz4 <jeffluiz97@gmail.com>
2021-12-05 22:24:50 +0100

    Vai logo

--- main.c
+++ main.c
@@ -1,32 +1,30 @@
 void	rush(int x, int y);
 void	ft_putchar(char c);
-int	read_value(char *arg);
+int		read_value(char *arg);
 
 int	main(int argc, char *argv[])
 {
-	int x;
-	int y;
-	int i;
-	int num;
+	int	x;
+	int	y;
+	int	i;
+	int	num;
 
 	num = 0;
 	i = 0;
-
 	if (argc == 3)
 	{
 		x = read_value(argv[1]);
 		y = read_value(argv[2]);
 		rush(x, y);
 	}
-
 	return (0);
 }
 
 int	read_value(char *arg)
 {
-	int value;
-	int number;
-	int i;
+	int	value;
+	int	number;
+	int	i;
 
 	number = 0;
 	i = 0;
--- rush00.c
+++ rush00.c
@@ -1,65 +1,62 @@
+void	ft_putchar(char c);
+void	column(int x, char a_edge, char b_edge, char mid_c, char line_b);
+void	line(int x, int y, char space, char line_b);
 
-void    ft_putchar(char c);
-void    column(int x, char a_edge, char b_edge, char mid_c, char line_b);
-void    line(int x, int y, char space, char line_b);
-
-void    rush(int x, int y)
+void	rush(int x, int y)
 {
-    char mid_c;
-    char a_edge;
-    char b_edge;
-    char line_b;
-    char space;
+	char	mid_c;
+	char	a_edge;
+	char	b_edge;
+	char	line_b;
+	char	space;
 
-    mid_c = '-';
-    a_edge = 'o';
-    b_edge = 'o';
-    line_b = '\n';
-    space = ' ';
-    if (x > 0 && y > 0)
-    {
-        column(x, a_edge, b_edge, mid_c, line_b);
-        line(x, y, space, line_b);
-        if (y > 1)
-        {
-            column(x, a_edge, b_edge, mid_c, line_b);
-        }
-    }
+	mid_c = '-';
+	a_edge = 'o';
+	b_edge = 'o';
+	line_b = '\n';
+	space = ' ';
+	if (x > 0 && y > 0)
+	{
+		column(x, a_edge, b_edge, mid_c, line_b);
+		line(x, y, space, line_b);
+		if (y > 1)
+		{
+			column(x, a_edge, b_edge, mid_c, line_b);
+		}
+	}
 }
 
-// Cria a largura
-void    column(int x, char a_edge, char b_edge, char mid_c, char line_b)
+void	column(int x, char a_edge, char b_edge, char mid_c, char line_b)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    ft_putchar(a_edge);
-    if (x >= 2)
-    {
-    while (i < (x - 2))
-    {
-        ft_putchar(mid_c);
-        i++;
-    }
-    ft_putchar(b_edge);
-    }
-    ft_putchar(line_b);
+	i = 0;
+	ft_putchar(a_edge);
+	if (x >= 2)
+	{
+		while (i < (x - 2))
+		{
+			ft_putchar(mid_c);
+			i++;
+		}
+		ft_putchar(b_edge);
+	}
+	ft_putchar(line_b);
 }
 
-// Cria a linha
-void    line(int x, int y, char space, char line_b)
+void	line(int x, int y, char space, char line_b)
 {
-    int i;
-    char mid_p;
-    mid_p = '|';
+	int		i;
+	char	mid_p;
 
-    i = 0;
-    if (y >= 2)
-    {
-        while (i < (y - 2))
-        {
-            column(x, mid_p, mid_p, space, line_b);
-            i++;
-        }
-    }
+	mid_p = '|';
+	i = 0;
+	if (y >= 2)
+	{
+		while (i < (y - 2))
+		{
+			column(x, mid_p, mid_p, space, line_b);
+			i++;
+		}
+	}
 }
